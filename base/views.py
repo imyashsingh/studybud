c572f86d473b15d63438e5e9592c9f8ccbc92162
imyashsingh <yash.singh@gmail.com>
2025-07-07 11:34:44 +0530

    Add base app with initial configurations, views, templates, and URL routing

---
+++ base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    context={}
+    return render(request, 'base/home.html',context)
+
+
+def room(request):
+    context={}
+    return render(request, 'base/room.html',context)
