057e975780a8560a9253497020808e3b57a0aecf
imyashsingh <yash.singh@gmail.com>
2025-07-07 11:57:57 +0530

    Refactor templates and views for room management; update URL patterns and enhance home template display

--- base/views.py
+++ base/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms=[
+    {'id':1,'name':'Learn Python'},
+    {'id':2,'name':'Learn Django'},
+]
+
 
 def home(request):
-    context={}
+    context={'rooms':rooms}
     return render(request, 'base/home.html',context)
 
 
-def room(request):
-    context={}
+def room(request,pk):
+    room=rooms[int(pk)-1]
+    context={'room':room}
     return render(request, 'base/room.html',context)
